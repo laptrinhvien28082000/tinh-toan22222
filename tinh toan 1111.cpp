d65b93b6a705985e4e9a153b4eb1b54a5ab72fa6
Dũng <57266655+laptrinhvien28082000@users.noreply.github.com>
2019-11-02 16:56:40 +0700

    Add files via upload

---
+++ tinh toan 1111.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+int main()
+{int n,A=0;
+char luachon;
+cout<<("Chao mung cac ban den voi rap xiec cua nhung chu he!\a\n");
+	do
+	{
+		cout << " Xin moi cu nhap gia tri n????\n: ";
+		cin >> n;
+		if (n < 1000)
+		{
+			cout << " Nhap cung ngu dm! Nhap lai n>1000 ?\a\n" << endl;}}
+		while (n < 1000);
+		
+		
+		
+		
+		
+	
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{ A += i ;}}
+	cout << " Lan sau nho nhap dung vao! Tong A can tim la:= " << A << endl;
+	cout<<("CAM ON DA XEM BUOI BIEU DIEN!!!!!!!!");
+
+	{
+	
+	cout<<("The lan sau co di xem he tiep khong?\n");
+cout<<("a.co\nb.khong\n");
+cout<<("Baby chon dap an nao?");
+luachon = getchar();
+cin>>luachon;
+switch(luachon)
+{case'a':
+
+	{cout<<"a" << endl;
+	cout<<("\n ngoan dung la con trai cua ta haha!");
+	break;}
+
+case'b':
+{cout<<" b " <<endl;
+cout<<("\n\a the thoi bye!!!!!");
+break;}
+
+return 0;}}}
+	
+	        
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
